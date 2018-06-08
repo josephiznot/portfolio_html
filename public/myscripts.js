2ba3a2b75337aa584b66f98d2f579657f7236e06
Joseph Anderson <josephiznot@gmail.com>
2018-06-07 19:53:50 -0500

    deleted some text

--- public/myscripts.js
+++ public/myscripts.js
@@ -29,21 +29,16 @@ const closeMenu = () => {
   openMenu = !openMenu;
 };
 const scrollToHome = () => {
-  console.trace(window.innerWidth);
-  if (window.innerWidth <= 320) {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  } else if (window.innerWidth <= 768) {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  } else if (window.innerWidth >= 1024) {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }
+  window.scrollTo({ top: 0, behavior: "smooth" });
 };
 const scrollToAbout = () => {
   if (window.innerWidth <= 320) {
     window.scrollTo({ top: 430, behavior: "smooth" });
   } else if (window.innerWidth <= 768) {
     window.scrollTo({ top: 430, behavior: "smooth" });
-  } else if (window.innerWidth >= 1024) {
+  } else if (window.innerWidth < 1024) {
+    window.scrollTo({ top: 436, behavior: "smooth" });
+  } else if (window.innerWidth > 1024) {
     window.scrollTo({ top: 603, behavior: "smooth" });
   }
 };
@@ -51,17 +46,21 @@ const scrollToProjects = () => {
   if (window.innerWidth <= 320) {
     window.scrollTo({ top: 2330, behavior: "smooth" });
   } else if (window.innerWidth <= 768) {
+    window.scrollTo({ top: 2331, behavior: "smooth" });
+  } else if (window.innerWidth < 1024) {
     window.scrollTo({ top: 1690, behavior: "smooth" });
-  } else if (window.innerWidth >= 1024) {
+  } else if (window.innerWidth > 1024) {
     window.scrollTo({ top: 1862, behavior: "smooth" });
   }
 };
 const scrollToSkills = () => {
   if (window.innerWidth <= 320) {
     window.scrollTo({ top: 1429, behavior: "smooth" });
-  } else if (window.innerWidth <= 768) {
-    window.scrollTo({ top: 1190, behavior: "smooth" });
-  } else if (window.innerWidth >= 1024) {
+  } else if (window.innerWidth < 768) {
+    window.scrollTo({ top: 1432, behavior: "smooth" });
+  } else if (window.innerWidth < 1024) {
+    window.scrollTo({ top: 1189, behavior: "smooth" });
+  } else if (window.innerWidth > 1024) {
     window.scrollTo({ top: 1361, behavior: "smooth" });
   }
 };
@@ -69,8 +68,10 @@ const scrollToContact = () => {
   if (window.innerWidth <= 320) {
     window.scrollTo({ top: 4059, behavior: "smooth" });
   } else if (window.innerWidth <= 768) {
-    window.scrollTo({ top: 2988, behavior: "smooth" });
-  } else if (window.innerWidth >= 1024) {
+    window.scrollTo({ top: 4004, behavior: "smooth" });
+  } else if (window.innerWidth < 1024) {
+    window.scrollTo({ top: 3160, behavior: "smooth" });
+  } else if (window.innerWidth > 1024) {
     window.scrollTo({ top: 3160, behavior: "smooth" });
   }
 };
